d8bf2ab0a5ef889d77575c06d1161b4789560c88
unknown <769613912@qq.com>
2021-10-19 22:04:34 +0800

    静态资源引入完成

--- src/main/java/com/lrm/web/IndexController.java
+++ src/main/java/com/lrm/web/IndexController.java
@@ -4,16 +4,19 @@ import com.lrm.NotFoundException;
 import org.hibernate.annotations.NotFound;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
     public String index(){
-//        int i = 9/0;
-        String blog = null;
-        if(blog==null){
-            throw  new NotFoundException("博客不存在");
-        }
+        System.out.println("-----------index-----------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        System.out.println("-----------blog-----------");
+        return "blog";
     }
 }
