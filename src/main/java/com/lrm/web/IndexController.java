459386d9769b86dd3e0199c142c2d110cf41fbd9
unknown <769613912@qq.com>
2021-10-31 11:56:38 +0800

    增加全局搜索内容标题描述

--- src/main/java/com/lrm/web/IndexController.java
+++ src/main/java/com/lrm/web/IndexController.java
@@ -1,12 +1,14 @@
 package com.lrm.web;
 
 import com.lrm.NotFoundException;
+import com.lrm.po.Blog;
 import com.lrm.service.BlogService;
 import com.lrm.service.TagService;
 import com.lrm.service.TypeService;
 import com.lrm.vo.BlogQuery;
 import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -36,6 +40,13 @@ public class IndexController {
     public String blog(){
         System.out.println("-----------blog-----------");
         return "blog";
+    }
+    @PostMapping("/search")
+    public String search(@PageableDefault(size=8,sort = {"updateTime"},direction = Sort.Direction.DESC) Pageable pageable,@RequestParam String query, Model model){
+        model.addAttribute("page",blogService.listBlog("%"+query+"%",pageable));
+        model.addAttribute("query",query);
+
+        return "search";
     }
 
     @GetMapping("/index/{id}")
